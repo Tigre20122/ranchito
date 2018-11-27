5a629b9f55a5b2355d4db7b4c88a730123b18420
Tigre20122 <33558674+Tigre20122@users.noreply.github.com>
2018-11-26 22:26:23 -0500

    hey jorge bienvenido al proyecto rancho

---
+++ rancho/rancho/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace rancho
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ rancho/rancho/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(rancho.Startup))]
+namespace rancho
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
